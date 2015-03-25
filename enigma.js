2f58665f9f2ec50fa36b98fb56327dcad50e30aa
kcypher13 <cypher@ftad.fr>
2015-03-25 09:43:47 +0100

    creation d'un tableau de correspondance

--- enigma.js
+++ enigma.js
@@ -2,6 +2,45 @@
 
 function Enigma(){
 
+	this.correspondingArray = [];
+	this.correspondingArray['a'] = 'j';
+	this.correspondingArray['b'] = 'd';
+	this.correspondingArray['c'] = 'w';
+	this.correspondingArray['d'] = 'o';
+	this.correspondingArray['e'] = 'v';
+	this.correspondingArray['f'] = 'a';
+	this.correspondingArray['g'] = 'r';
+	this.correspondingArray['h'] = 'i';
+	this.correspondingArray['i'] = 'g';
+	this.correspondingArray['j'] = ' ';
+	this.correspondingArray['k'] = 'x';
+	this.correspondingArray['l'] = 'u';
+	this.correspondingArray['m'] = 'n';
+	this.correspondingArray['n'] = 's';
+	this.correspondingArray['o'] = 'y';
+	this.correspondingArray['p'] = 'q';
+	this.correspondingArray['q'] = 'c';
+	this.correspondingArray['r'] = 'p';
+	this.correspondingArray['s'] = '\'';
+	this.correspondingArray['t'] = 'f';
+	this.correspondingArray['u'] = 'm';
+	this.correspondingArray['v'] = 'z';
+	this.correspondingArray['w'] = 't';
+	this.correspondingArray['x'] = 'k';
+	this.correspondingArray['y'] = 'h';
+	this.correspondingArray['z'] = 'b';
+	this.correspondingArray[' '] = 'e';
+	this.correspondingArray['\''] = 'l';
+	this.correspondingArray['.'] = '.';
+	this.correspondingArray[','] = ',';
+	this.correspondingArray[':'] = ':';
+	this.correspondingArray[';'] = ';';
+
+
+
+
+
+
 	this.encrypt = function(msgClear)
 	{
 		return "jdwovarig xunsyqcp'fmztkhbel.,:;";
