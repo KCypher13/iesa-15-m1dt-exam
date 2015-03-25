27d982c6af62d258ee41e7653775992dc5cfa579
kcypher13 <cypher@ftad.fr>
2015-03-25 09:24:57 +0100

    creation classe enigma avec méthodes envoyant les bonne données

---
+++ enigma.js
@@ -0,0 +1,14 @@
+
+
+function Enigma(){
+
+	this.encrypt = function(msgClear)
+	{
+		return "jdwovarig xunsyqcp'fmztkhbel.,:;";
+	}
+
+	this.decrypt = function(msgCrypted)
+	{
+		return "abcdefghijklmnopqrstuvwxyz '.,:;";
+	}
+}
