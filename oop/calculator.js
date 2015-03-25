98977c5c8000c33f126d9e8ef10712c18b23c5da
kcypher13 <cypher@ftad.fr>
2015-03-25 10:31:32 +0100

    creation des tests

---
+++ oop/calculator.js
@@ -0,0 +1,24 @@
+
+
+function Calculator(){
+
+	this.result = 0;
+
+
+	this.add = function (nbr){
+
+	}
+
+	this.minus = function (nbr){
+
+	}
+
+	this.devide = function (nbr){
+
+	}
+
+	this.multiply = function (nbr){
+
+	}
+
+}
