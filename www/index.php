b8a99321f6504522ab0be7a7bc65bef560f336cd
kcypher13 <cypher@ftad.fr>
2015-03-25 10:53:18 +0100

    creation skeleton pour site php

---
+++ www/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Index - Planetarium</title>
+	<link rel="stylesheet" media="min-width=960px"href="desktop.css">
+	<link rel="stylesheet" media="max-width=960px"href="mobile.css">
+</head>
+<body>
+
+<header></header>
+<nav></nav>
+<section></section>
+<footer></footer>
+
+	
+</body>
+</html>
