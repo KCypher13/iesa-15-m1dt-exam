77575951122d6d3b22c618caf0a70150a51f6fb2
kcypher13 <cypher@ftad.fr>
2015-03-25 11:29:07 +0100

    listing des planetes

--- www/index.php
+++ www/index.php
@@ -12,7 +12,34 @@
 	<h1>Planetarium</h1>
 </header>
 <nav></nav>
-<section></section>
+<section>
+	<?php
+
+		$planetes = fopen('planete.json', 'r');
+		$planetes = fread($planetes, filesize('planete.json'));
+		$planetes = json_decode($planetes);
+		$html = "";
+		
+		foreach ($planetes as $key => $value) {
+
+			$html .= '<div>';
+			$html .= '<h2>'.$key.'</h2>';
+			$html .= '<img src="img/'.$value->image.'"/>';
+			$html .= '<ul>';
+			$html .= '<li>Rayon : '.$value->rayon.'</li>';
+			$html .= '<li>Distance du soleil : '.$value->distance.'</li>';
+			$html .= '<li>Description : '.substr($value->description, 0, 100).'</li>';
+			$html .= '</ul>';
+			$html .= '<a href=detail.php?id="'.$key.'">plus de details</a>';
+			$html .= '</div>';
+
+		}
+		echo $html;
+
+	?>
+
+
+</section>
 <footer></footer>
 
 	
